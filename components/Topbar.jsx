e891a1c5b2d0dc9673e6b3f8683cd0b28892df2e
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-03-09 15:52:35 +0000

    Add files via upload

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -3,13 +3,13 @@ import { FaPhone, FaEnvelope } from 'react-icons/fa';
 const Topbar = () => {
   return (
     <div className='w-full py-1 px-2 bg-gray-200 text-slate-700 flex items-cente r justify-center gap-16'>
-      <div className='flex gap-2 items-center justify-center'>
-        <span className='bg-white rounded-full p-2'>
+      <div className='flex gap-2 items-center justify-center sm:text-base text-sm'>
+        <span className='bg-white  rounded-full p-2'>
           <FaPhone />
         </span>
         Dr. Drage Ljočić 3, Beograd | 069 288 71 94
       </div>
-      <div className='flex gap-2 items-center justify-center'>
+      <div className='flex gap-2 items-center justify-center sm:text-base text-sm'>
         <span className='bg-white rounded-full p-2'>
           <FaEnvelope />
         </span>
