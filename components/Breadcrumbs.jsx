71650ce0c19a906084fce34ca0a3c52a0b50ae9d
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-04-03 11:09:41 +0200

    Add files via upload

--- components/Breadcrumbs.jsx
+++ components/Breadcrumbs.jsx
@@ -2,32 +2,36 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 const BreadcrumbExample = () => {
   const router = useRouter();
+
+  const pathSnippets = router.asPath.split('/').filter((i) => i);
+
+  const urls = pathSnippets.map((snippet, idx) => {
+    const url = `/${snippet}`;
+    if (snippet === 'proizvodi') snippet = 'производи';
+    if (snippet === 'brendovi') snippet = 'брендови';
+    if (snippet === 'o-nama') snippet = 'о нама';
+    if (snippet === 'kontakt') snippet = 'контакт';
+    if (snippet === 'akcije') snippet = 'акције';
+    if (snippet === 'psi') snippet = 'пси';
+    if (snippet === 'macke') snippet = 'мачке';
+    if (snippet === 'ptice') snippet = 'птице';
+    if (snippet === 'akvaristika') snippet = 'акваристика';
+    if (snippet === 'male-zivotinje') snippet = 'мале животиње';
+    if (snippet.includes('#')) return snippet.slice(0, 1);
+    return (
+      <Link key={idx} href={url}>
+        <a>
+          <span className='mr-2'>/</span> {snippet}
+        </a>
+      </Link>
+    );
+  });
   return (
     <div className='breadcrumbs w-4/5 m-auto text-base flex gap-2 text-gray-500 uppercase cursor-pointer border-b border-gray-300 p-2'>
       <Link href='/'>
         <a>почетна</a>
       </Link>
-      {router.asPath
-        .split('/')
-        .slice(1)
-        .map((link, key) => {
-          if (link === 'proizvodi') link = 'производи';
-          if (link === 'brendovi') link = 'брендови';
-          if (link === 'o_nama') link = 'о нама';
-          if (link === 'kontakt') link = 'контакт';
-          if (link === 'akcije') link = 'акције';
-          if (link === 'psi') link = 'пси';
-          if (link === 'macke') link = 'мачке';
-          if (link === 'ptice') link = 'птице';
-          if (link === 'akvaristika') link = 'акваристика';
-          if (link === 'male-zivotinje') link = 'мале животиње';
-
-          return (
-            <p key={key} onClick={() => router.back()} className=''>
-              {link && <span className='mx-2'>/</span>} {link}
-            </p>
-          );
-        })}
+      {urls.map((url) => url)}
     </div>
   );
 };
@@ -38,3 +42,20 @@ export default BreadcrumbExample;
 // { href: '/brendovi', name: 'брендови' },
 // { href: '/o-nama', name: 'о нама' },
 // { href: '/kontakt', name: 'контакт' },
+
+// const breadcrumbNameMap = {
+//   '/': 'почетна',
+//   '/proizvodi': 'производи',
+//   '/brendovi': 'брендови',
+//   '/o-nama': 'о нама',
+//   '/kontakt': 'контакт',
+//   '/akcije': 'акције',
+//   '/psi': 'пси',
+//   '/macke': 'мачке',
+//   '/ptice': 'птице',
+//   '/akvaristika': 'акваристика',
+//   '/male-zivotinje': 'мале животиње',
+// };
+
+// const cir_url = Object.values(breadcrumbNameMap);
+// console.log(cir_url);
