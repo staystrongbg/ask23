7cfcb88fe631f1d25039e866b1b5ea9df80d5038
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-04-08 12:14:01 +0200

    Add files via upload

--- components/Placeholder.jsx
+++ components/Placeholder.jsx
@@ -3,7 +3,7 @@ import { H1 } from '../components/utils';
 const Placeholder = ({ item, width = 240, aspect = 'square' }) => {
   return (
     <span
-      className={` hover:opacity-60 cursor-pointer border border-gray-400 w-60 grid items-center bg-indigo-100 aspect-square `}
+      className={` hover:opacity-60 cursor-pointer border border-gray-400 w-[${width}px] grid items-center bg-indigo-100 aspect-square `}
     >
       <a href='#'>
         <H1>brend {item}</H1>
